c37871b3736cc7c25266d2a09baeb87295f63327
= <=>
2020-04-13 13:33:47 +0700

    search layanan, hewan complete

--- app/src/main/java/com/example/kouveepetshop/ui/hewan/HewanViewFragment.java
+++ app/src/main/java/com/example/kouveepetshop/ui/hewan/HewanViewFragment.java
@@ -1,18 +1,30 @@
 package com.example.kouveepetshop.ui.hewan;
 
+import android.app.AlertDialog;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.kouveepetshop.R;
+import com.example.kouveepetshop.SplashScreen;
+import com.example.kouveepetshop.UserSharedPreferences;
 import com.example.kouveepetshop.api.ApiClient;
 import com.example.kouveepetshop.api.ApiHewan;
 import com.example.kouveepetshop.model.HewanModel;
@@ -32,7 +44,15 @@ public class HewanViewFragment extends Fragment {
     private RecyclerView recyclerView;
     private HewanRecycleAdapter hewanRecycleAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private SearchView searchView = null;
+    private SearchView.OnQueryTextListener queryTextListener;
     View myView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -47,6 +67,43 @@ public class HewanViewFragment extends Fragment {
         showAllHewan();
 
         return myView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+
+        MenuItem searchItem = menu.findItem(R.id.SearchTxt);
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+        queryTextListener = new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                hewanRecycleAdapter.getFilter().filter(newText);
+                return true;
+            }
+        };
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.LogOut:
+                showDialog();
+                return true;
+            default:
+                break;
+        }
+
+        searchView.setOnQueryTextListener(queryTextListener);
+        return super.onOptionsItemSelected(item);
     }
 
     public void showAllHewan(){
@@ -72,5 +129,35 @@ public class HewanViewFragment extends Fragment {
                 Toast.makeText(getContext(), "Connection Problem", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    private void doLogout(){
+        UserSharedPreferences SP = new UserSharedPreferences(getActivity());
+        SP.spEditor.clear();
+        SP.saveSPBoolean(UserSharedPreferences.SP_ISLOGIN, false);
+        SP.spEditor.apply();
+        Toast.makeText(getActivity(), "Logout Success", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getActivity(), SplashScreen.class);
+        getActivity().finish();
+        startActivity(intent);
+    }
+
+    private void showDialog(){
+        AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
+        alertDialog.setTitle("Warning");
+        alertDialog.setMessage("Are you sure want to logout ?");
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                doLogout();
+            }
+        });
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        alertDialog.show();
     }
 }
--- app/src/main/java/com/example/kouveepetshop/ui/layanan/LayananViewFragment.java
+++ app/src/main/java/com/example/kouveepetshop/ui/layanan/LayananViewFragment.java
@@ -1,5 +1,10 @@
 package com.example.kouveepetshop.ui.layanan;
 
+import android.app.AlertDialog;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,13 +15,17 @@ import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.kouveepetshop.OwnerActivity;
 import com.example.kouveepetshop.R;
+import com.example.kouveepetshop.SplashScreen;
+import com.example.kouveepetshop.UserSharedPreferences;
 import com.example.kouveepetshop.api.ApiClient;
 import com.example.kouveepetshop.api.ApiLayanan;
 import com.example.kouveepetshop.model.LayananModel;
@@ -36,7 +45,15 @@ public class LayananViewFragment extends Fragment {
     private RecyclerView recyclerView;
     private LayananRecycleAdapter layananRecycleAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    private SearchView searchView = null;
+    private SearchView.OnQueryTextListener queryTextListener;
     View myView;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -58,9 +75,11 @@ public class LayananViewFragment extends Fragment {
         super.onCreateOptionsMenu(menu, inflater);
 
         MenuItem searchItem = menu.findItem(R.id.SearchTxt);
-        SearchView searchView = (SearchView) searchItem.getActionView();
+        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        searchView = (SearchView) searchItem.getActionView();
 
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+        queryTextListener = new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
                 return false;
@@ -69,12 +88,24 @@ public class LayananViewFragment extends Fragment {
             @Override
             public boolean onQueryTextChange(String newText) {
                 layananRecycleAdapter.getFilter().filter(newText);
-                return false;
+                return true;
             }
-        });
+        };
     }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.LogOut:
+                showDialog();
+                return true;
+            default:
+                break;
+        }
 
+        searchView.setOnQueryTextListener(queryTextListener);
+        return super.onOptionsItemSelected(item);
+    }
 
     public void showAllLayanan(){
         ApiLayanan apiLayanan = ApiClient.getClient().create(ApiLayanan.class);
@@ -99,5 +130,35 @@ public class LayananViewFragment extends Fragment {
                Toast.makeText(getContext(), "Connection Problem", Toast.LENGTH_SHORT).show();
            }
        });
+    }
+
+    private void doLogout(){
+        UserSharedPreferences SP = new UserSharedPreferences(getActivity());
+        SP.spEditor.clear();
+        SP.saveSPBoolean(UserSharedPreferences.SP_ISLOGIN, false);
+        SP.spEditor.apply();
+        Toast.makeText(getActivity(), "Logout Success", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(getActivity(), SplashScreen.class);
+        getActivity().finish();
+        startActivity(intent);
+    }
+
+    private void showDialog(){
+        AlertDialog alertDialog = new AlertDialog.Builder(getActivity()).create();
+        alertDialog.setTitle("Warning");
+        alertDialog.setMessage("Are you sure want to logout ?");
+        alertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, "Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                doLogout();
+            }
+        });
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        alertDialog.show();
     }
 }
